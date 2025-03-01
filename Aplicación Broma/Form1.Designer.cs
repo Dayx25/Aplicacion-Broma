c725310dafca7fe2a4c8bd862ac2dcf006b96405
escob <escob@DESKTOP-LL5TP6L>
2025-03-01 16:22:17 -0600

    Agregar archivos de proyecto.

---
+++ Aplicación Broma/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Aplicación_Broma
+{
+    partial class AplicacionBroma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotonBroma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BotonBroma
+            // 
+            this.BotonBroma.Location = new System.Drawing.Point(340, 257);
+            this.BotonBroma.Name = "BotonBroma";
+            this.BotonBroma.Size = new System.Drawing.Size(147, 72);
+            this.BotonBroma.TabIndex = 0;
+            this.BotonBroma.Text = "¡¡¡¡ HAZ CLICK AQUÍ !!!!";
+            this.BotonBroma.UseVisualStyleBackColor = true;
+            this.BotonBroma.MouseEnter += new System.EventHandler(this.BotonBroma_MouseEnter);
+            // 
+            // AplicacionBroma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Aplicación_Broma.Properties.Resources.riacrdoarjona4k;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BotonBroma);
+            this.Name = "AplicacionBroma";
+            this.Text = "AplicacionBroma";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotonBroma;
+    }
+}
+
---
+++ Aplicación Broma/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicación_Broma
+{
+    public partial class AplicacionBroma : Form
+    {
+        public AplicacionBroma()
+        {
+            InitializeComponent();
+        }
+
+        int A = 0, B = 0, beforeA = 0, beforeB = 0;
+        bool ext = true;
+
+        private void BotonBroma_MouseEnter(object sender, EventArgs e)
+        {
+            ext = true;
+
+            while (ext)
+            {
+                A = random.Next(42, 400);
+
+                if (beforeA + 100 < A || beforeA - 100 < A)
+                {
+                    ext = false;
+                }
+            }
+
+            ext = true;
+
+            while (ext)
+            {
+                B = random.Next(35, 400);
+
+                if (beforeB + 100 < B || beforeB - 100 < B)
+                {
+                    ext = false;
+                }
+            }
+
+            BotonBroma.Location = new Point(A, B);
+            beforeA = A;
+            beforeB = B;
+        }
+
+        Random random = new Random();
+    }
+}
